73b1ab1d36da7741c14b81fe375a5568db8c009e
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-21 16:46:48 +0300

    Set ViewController as rootViewController

--- CustomAlertController/CustomAlertController/Screens/ViewController.swift
+++ CustomAlertController/CustomAlertController/Screens/ViewController.swift
@@ -4,7 +4,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("There is a main screen")
+        
     }
 
 
