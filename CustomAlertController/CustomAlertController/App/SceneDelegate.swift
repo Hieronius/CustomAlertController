716d348ff5a2e378938745d265808f598cad5467
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-21 16:37:37 +0300

    Remove storyboard and unused code

--- CustomAlertController/CustomAlertController/App/SceneDelegate.swift
+++ CustomAlertController/CustomAlertController/App/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  CustomAlertController
-//
-//  Created by Арсентий Халимовский on 21.02.2024.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
--- CustomAlertController/CustomAlertController/Screens/ViewController.swift
+++ CustomAlertController/CustomAlertController/Screens/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  CustomAlertController
-//
-//  Created by Арсентий Халимовский on 21.02.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
